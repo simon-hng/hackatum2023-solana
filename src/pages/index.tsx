70d828036f8dcc0dec183d882a849e1998aa0072
simonhng <simon.huang@tum.de>
2023-11-19 03:27:27 +0100

    add landing background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             className="hero min-h-screen"
             style={{
               backgroundImage:
-                "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+                "url(https://cdn.discordapp.com/attachments/1171074422745075823/1175623121903030282/DALLE_2023-11-19_03.26.24_-_A_minimalistic_retrowave-style_cover_image_for_a_web3_crowdfunding_platform._The_image_features_a_simple_sleek_design_with_a_dark_background_and_neon.png?ex=656be75a&is=6559725a&hm=2d3e278f50cee974dfda040b81038321d17db1675f06c50277d99f77e433688b&)",
             }}
           >
             <div className="hero-overlay bg-opacity-60"></div>
