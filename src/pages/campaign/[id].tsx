ab87074db003065f03b4e4b3b3728a29786c126b
simonhng <simon.huang@tum.de>
2023-11-19 09:57:52 +0100

    ignore eslint and ts errors

--- src/pages/campaign/[id].tsx
+++ src/pages/campaign/[id].tsx
@@ -1,22 +1,24 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { Layout } from "~/components/layout";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function CampaignPage() {
-
   const handleContribute = async () => {
     const campaignId = router.query.id; // Get campaign ID from URL
     const res = await fetch(`/api/solana/contribute`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ campaignId: campaignId, amount: contributionAmount}),
+      body: JSON.stringify({
+        campaignId: campaignId,
+        amount: contributionAmount,
+      }),
     });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const data = await res.json();
-    console.log(data)
+    console.log(data);
     if (data.txhash) {
       setTxHash(data.txhash); // Set the transaction hash
       setShowAlert(true); // Show the alert
@@ -27,19 +29,18 @@ export default function CampaignPage() {
   };
 
   const router = useRouter();
-  const [campaignInfo, setCampaignInfo] = useState({}); // State to store campaign info
-  const [contributionAmount, setContributionAmount] = useState('');
+  const [campaignInfo, setCampaignInfo] = useState<any>({}); // State to store campaign info
+  const [contributionAmount, setContributionAmount] = useState("");
   const [timeLeft, setTimeLeft] = useState(0);
-  const [txHash, setTxHash] = useState('');
+  const [txHash, setTxHash] = useState("");
   const [showAlert, setShowAlert] = useState(false);
-
 
   const fetchCampaignInfo = async () => {
     const campaignId = router.query.id; // Get campaign ID from URL
     const res = await fetch(`/api/solana/info`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ campaignId }),
     });
@@ -54,15 +55,15 @@ export default function CampaignPage() {
   const handlePayout = async () => {
     const campaignId = router.query.id; // Get campaign ID from URL
     const res = await fetch(`/api/solana/finalize`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ campaignId: campaignId}),
+      body: JSON.stringify({ campaignId: campaignId }),
     });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const data = await res.json();
-    console.log(data)
+    console.log(data);
 
     if (data.txhash) {
       setTxHash(data.txhash); // Set the transaction hash
@@ -80,10 +81,15 @@ export default function CampaignPage() {
     return now > deadline;
   };
 
-  const initializeCountdown = (deadline) => {
+  const initializeCountdown = (deadline: any) => {
     const deadlineDate = new Date(deadline * 1000); // Convert to milliseconds
     const now = new Date();
-    setTimeLeft(Math.max(deadlineDate - now, 0));
+    setTimeLeft(
+      Math.max(
+        (deadlineDate as unknown as number) - (now as unknown as number),
+        0,
+      ),
+    );
   };
 
   useEffect(() => {
@@ -95,14 +101,14 @@ export default function CampaignPage() {
     // Update time left every second
     const interval = setInterval(() => {
       const now = new Date();
-      setTimeLeft(prevTimeLeft => Math.max(prevTimeLeft - 1000, 0));
+      setTimeLeft((prevTimeLeft) => Math.max(prevTimeLeft - 1000, 0));
     }, 1000);
 
     return () => clearInterval(interval); // Clean up on unmount
   }, []);
 
-  const formatTimeLeft = (milliseconds) => {
-    if (milliseconds <= 0) return '0';
+  const formatTimeLeft = (milliseconds: any) => {
+    if (milliseconds <= 0) return "0";
     let seconds = Math.floor(milliseconds / 1000);
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
@@ -115,11 +121,11 @@ export default function CampaignPage() {
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
   };
 
-  const filteredContributors = (campaignInfo.contributors || []).filter(contributor =>
-      contributor.amount != 0
-  ).sort((a, b) => {
-    return b.contributedBalance - a.contributedBalance;
-  });
+  const filteredContributors = (campaignInfo?.contributors || [])
+    .filter((contributor) => contributor.amount != 0)
+    .sort((a, b) => {
+      return b.contributedBalance - a.contributedBalance;
+    });
 
   const images = [
     { url: "/openai-api.png", caption: "OpenAI API Key (0.1 SOL)" },
@@ -135,19 +141,35 @@ export default function CampaignPage() {
   return (
     <Layout>
       {showAlert && (
-          <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative my-3" role="alert">
-            <strong className="font-bold">Transaction Successful!</strong>
-            <span className="block sm:inline"> Check the transaction </span>
-            <a href={`https://explorer.solana.com/tx/${txHash}/?cluster=devnet`} className="underline" target="_blank" rel="noopener noreferrer">
-              here!
-            </a>
-            <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={() => setShowAlert(false)}>
-            <svg className="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <div
+          className="relative my-3 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700"
+          role="alert"
+        >
+          <strong className="font-bold">Transaction Successful!</strong>
+          <span className="block sm:inline"> Check the transaction </span>
+          <a
+            href={`https://explorer.solana.com/tx/${txHash}/?cluster=devnet`}
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here!
+          </a>
+          <span
+            className="absolute bottom-0 right-0 top-0 px-4 py-3"
+            onClick={() => setShowAlert(false)}
+          >
+            <svg
+              className="h-6 w-6 fill-current text-green-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
               <title>Close</title>
-              <path d="M14.348 14.859l-4.708-4.708 4.708-4.708a1.002 1.002 0 00-1.414-1.414L8.226 8.738l-4.708-4.708a1.002 1.002 0 10-1.414 1.414l4.708 4.708-4.708 4.708a1.002 1.002 0 101.414 1.414l4.708-4.708 4.708 4.708a1.002 1.002 0 001.414-1.414z"/>
+              <path d="M14.348 14.859l-4.708-4.708 4.708-4.708a1.002 1.002 0 00-1.414-1.414L8.226 8.738l-4.708-4.708a1.002 1.002 0 10-1.414 1.414l4.708 4.708-4.708 4.708a1.002 1.002 0 101.414 1.414l4.708-4.708 4.708 4.708a1.002 1.002 0 001.414-1.414z" />
             </svg>
           </span>
-          </div>
+        </div>
       )}
       <section>
         <article className="grid grid-cols-2 gap-8 p-8">
@@ -160,39 +182,49 @@ export default function CampaignPage() {
           />
           <div className="flex flex-col gap-3">
             <h1 className="text-bold text-4xl">Rent Money</h1>
-            <p>You think crypto is turbolent? I invented AGI and still got fired! :(</p>
+            <p>
+              You think crypto is turbolent? I invented AGI and still got fired!
+              :(
+            </p>
             <p>Willing to trade OpenAI API Keys!</p>
             {hasDeadlinePassed() ? (
-                // Render Pay out button after deadline
-                <button className="btn btn-success" onClick={handlePayout}>
-                  Pay out
-                </button>
+              // Render Pay out button after deadline
+              <button className="btn btn-success" onClick={handlePayout}>
+                Pay out
+              </button>
             ) : (
-                // Render Contribute button and input field before deadline
-                <div className="flex gap-2 items-center">
-                  <input
-                      type="number"
-                      placeholder="Contribution Amount"
-                      className="input input-bordered"
-                      value={contributionAmount}
-                      onChange={(e) => setContributionAmount(e.target.value)}
-                  />
-                  <button className="btn" onClick={() => handleContribute()}>
-                    Contribute
-                  </button>
-                </div>
+              // Render Contribute button and input field before deadline
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  placeholder="Contribution Amount"
+                  className="input input-bordered"
+                  value={contributionAmount}
+                  onChange={(e) => setContributionAmount(e.target.value)}
+                />
+                <button className="btn" onClick={() => handleContribute()}>
+                  Contribute
+                </button>
+              </div>
             )}
-            <p>Enter the amount in Lamport. Remember 1 SOL = 10^9 (100000000) Lamport.</p>
+            <p>
+              Enter the amount in Lamport. Remember 1 SOL = 10^9 (100000000)
+              Lamport.
+            </p>
           </div>
         </article>
         <div className="grid grid-cols-3 py-8">
           <div className="text-center">
-            <h2 className="text-2xl">{campaignInfo.totalContributed / 1000000000} SOL</h2>
+            <h2 className="text-2xl">
+              {campaignInfo.totalContributed / 1000000000} SOL
+            </h2>
             <p>Raised</p>
           </div>
 
           <div className="text-center">
-            <h2 className="text-2xl">{campaignInfo.threshold / 1000000000} SOL</h2>
+            <h2 className="text-2xl">
+              {campaignInfo.threshold / 1000000000} SOL
+            </h2>
             <p>Goal</p>
           </div>
 
@@ -202,43 +234,56 @@ export default function CampaignPage() {
           </div>
         </div>
         <div className="card mx-8 border-2 border-base-content bg-base-100 shadow-xl">
-          <h1 className="text-2xl text-center">Leaderboard</h1>
+          <h1 className="text-center text-2xl">Leaderboard</h1>
           <div className="overflow-x-auto">
             <table className="table">
               <thead>
-              <tr>
-                <th></th>
-                <th>Public Key</th>
-                <th>Amount Pledged (Lamport)</th>
-                <th></th>
-              </tr>
+                <tr>
+                  <th></th>
+                  <th>Public Key</th>
+                  <th>Amount Pledged (Lamport)</th>
+                  <th></th>
+                </tr>
               </thead>
               <tbody>
-              {filteredContributors.map((contributor, index) => (
+                {filteredContributors.map((contributor, index) => (
                   <tr key={index}>
                     <td>{index + 1}</td>
                     <td>{contributor.publicKey}</td>
                     <td>{contributor.contributedBalance}</td>
                     <td className="text-center">
-                      <span className="badge badge-ghost badge-sm">Confirmed</span>
+                      <span className="badge badge-ghost badge-sm">
+                        Confirmed
+                      </span>
                     </td>
                   </tr>
-              ))}
+                ))}
               </tbody>
             </table>
           </div>
         </div>
         <div className="card mx-8 my-4 border-2 border-base-content bg-base-100 shadow-xl">
-          <div className="flex overflow-x-auto py-4" style={{ maxHeight: "300px" }}>
+          <div
+            className="flex overflow-x-auto py-4"
+            style={{ maxHeight: "300px" }}
+          >
             {images.map((image, index) => (
-                <div key={index} className={`flex-none w-60 mx-2 ${isFirstImageGreyedOut() && index === 0 ? 'opacity-10' : ''}`}>
-                  <img src={image.url} alt={`Image ${index + 1}`} className="w-full h-auto rounded" />
-                  <p className="mt-2 text-center">{image.caption}</p>
-                </div>
+              <div
+                key={index}
+                className={`mx-2 w-60 flex-none ${
+                  isFirstImageGreyedOut() && index === 0 ? "opacity-10" : ""
+                }`}
+              >
+                <img
+                  src={image.url}
+                  alt={`Image ${index + 1}`}
+                  className="h-auto w-full rounded"
+                />
+                <p className="mt-2 text-center">{image.caption}</p>
+              </div>
             ))}
           </div>
         </div>
-
       </section>
     </Layout>
   );
