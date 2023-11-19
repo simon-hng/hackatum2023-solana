1a3c0930aa17cba8042fadabf81d4b1b9941dbea
simonhng <simon.huang@tum.de>
2023-11-19 05:13:11 +0100

    remove unused code

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -45,10 +45,6 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
